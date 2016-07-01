fb58787d429e5232f53c0bfb22b71fc4d4bbc3a2
Mark Wright <thepiercingarrow@gmail.com>
2016-07-01 07:22:51 -0400

    Update transform.c

--- src/usaco-training/transform.c
+++ src/usaco-training/transform.c
@@ -16,7 +16,7 @@ int main() {
   int N, i, j;
   char before[10][11], after[10][11];
 
-  getw(fin, &N);
+  scanf(fin, "%d", &N);
   for (i = 0; i < N; ++i)
     fscanf(fin, "%s\n", before[i]);
   for (i = 0; i < N; ++i)
